b4a018843a5b0ccd9f548ac6d9e119a8db6fb35b
Harvey Bal <harvey_bal@hotmail.co.uk>
2018-02-25 22:01:09 +0000

    Added Fragment class and tests

--- src/main/java/fragment/submissions/HarveyBal.java
+++ src/main/java/fragment/submissions/HarveyBal.java
@@ -5,19 +5,73 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class HarveyBal {
 
     public static void main(String args[]) throws IOException{
         try(BufferedReader in = new BufferedReader(new FileReader(args[0]))) {
-            in.lines()
-                    .map(HarveyBal::reassemble)
-                    .forEach(System.out::println);
+//            in.lines()
+//                    .map(HarveyBal::reassemble)
+//                    .forEach(System.out::println);
         }
     }
 
-    static String reassemble(String line){
-        List<String> fragments = Arrays.asList(line.split(";"));
-        return fragments.toString();
+//    static String reassemble(String line){
+//        List<String> fragments = Arrays.asList(line.split(";"));
+//        String result = findOverlap(fragments);
+//        return result;
+//    }
+
+//    static String findOverlap(List<String> fragments){
+//        for(int i = 0; i<fragments.size(); i++){
+//            String fragmentAlpha = fragments.get(i);
+//            for(int j = 0; j<fragments.size(); j++){
+//                String fragmentBeta = fragments.get(i);
+//                if(i!=j){
+//
+//                }
+//            }
+//        }
+//    }
+
+    public class Fragment {
+
+        private String internal;
+
+        public Fragment(String internal) {
+            this.internal=internal;
+        }
+
+        public String getInternal(){
+            return internal;
+        }
+
+        public String getOverlap(Fragment fragmentBeta){
+            String resultRight = "";
+            String resultLeft = "";
+            String stringPrime = this.getInternal();
+            String stringBeta = fragmentBeta.getInternal();
+            //Check Right
+            for( int i = Math.min(stringPrime.length(), stringBeta.length()); ; i--){
+                if(stringPrime.endsWith(stringBeta.substring(0,i))){
+                    resultRight = stringBeta.substring(0,i);
+                    break;
+                }
+            }
+            //Check Left
+            for( int i = 0; i<stringBeta.length(); i++){
+                if(stringPrime.startsWith(stringBeta.substring(i,stringBeta.length()))){
+                    resultLeft = stringBeta.substring(i,stringBeta.length());
+                    break;
+                }
+            }
+            if(resultRight.length() > resultLeft.length()){
+                return resultRight;
+            } else {
+                return resultLeft;
+            }
+        }
     }
+
 }
---
+++ src/test/java/HarveyBalTest.java
@@ -0,0 +1,47 @@
+import fragment.submissions.HarveyBal;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HarveyBalTest{
+
+    @Test
+    public void testFragmentGetOverlapReturnsStringFromRight(){
+        HarveyBal hb = new HarveyBal();
+        HarveyBal.Fragment fragmentPrime = hb.new Fragment("ABC");
+        HarveyBal.Fragment fragmentBeta = hb.new Fragment("BCZ");
+        Assert.assertEquals("BC", fragmentPrime.getOverlap(fragmentBeta));
+    }
+
+    @Test
+    public void testFragmentGetOverlapReturnsStringFromLeft(){
+        HarveyBal hb = new HarveyBal();
+        HarveyBal.Fragment fragmentPrime = hb.new Fragment("ABC");
+        HarveyBal.Fragment fragmentBeta = hb.new Fragment("ZAB");
+        Assert.assertEquals("AB", fragmentPrime.getOverlap(fragmentBeta));
+    }
+
+    @Test
+    public void testFragmentGetOverlapReturnsStringFromLeftLarger(){
+        HarveyBal hb = new HarveyBal();
+        HarveyBal.Fragment fragmentPrime = hb.new Fragment("ABCDEF");
+        HarveyBal.Fragment fragmentBeta = hb.new Fragment("EFABC");
+        Assert.assertEquals("ABC", fragmentPrime.getOverlap(fragmentBeta));
+    }
+
+    @Test
+    public void testFragmentGetOverlapReturnsStringFromRightLarger(){
+        HarveyBal hb = new HarveyBal();
+        HarveyBal.Fragment fragmentPrime = hb.new Fragment("ABCDEF");
+        HarveyBal.Fragment fragmentBeta = hb.new Fragment("DEFAB");
+        Assert.assertEquals("DEF", fragmentPrime.getOverlap(fragmentBeta));
+    }
+
+    @Test
+    public void testFragmentGetOverlapReturnsEmpty(){
+        HarveyBal hb = new HarveyBal();
+        HarveyBal.Fragment fragmentPrime = hb.new Fragment("ABCD");
+        HarveyBal.Fragment fragmentBeta = hb.new Fragment("ZBCY");
+        Assert.assertEquals("", fragmentPrime.getOverlap(fragmentBeta));
+    }
+
+}
